32fe90ef63d8fa7b8c9a6b6e6375182ccc2ebc1c
Akshay H <akshayhuliyaru2005@gmail.com>
2026-05-01 22:27:47 +0530

    Update:updated the social links

--- Myportfoliomain/settings.py
+++ Myportfoliomain/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import dj_database_url
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2cjjdw2#@#-x5vtpv$m*^^0h&cq2tp=n1x1)$g#x$mdveb902v'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'Myportfoliomain.urls'
@@ -65,10 +66,10 @@ WSGI_APPLICATION = 'Myportfoliomain.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
@@ -114,6 +115,7 @@ STATICFILES_DIRS=[BASE_DIR / "static",]
 #Static Root for deployment
 STATIC_ROOT=BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Media url
 MEDIA_URL='/media/'
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.core.mail import send_mail
 from . models import Projects
+from django.core.mail import EmailMessage
+from django.conf import settings
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 def home(request):
     projects=Projects.objects.all()
     if request.method=='POST':
@@ -9,11 +14,13 @@ def home(request):
         email=request.POST.get('email')
         message=request.POST.get('message')
         
-        send_mail(
-            f"Message from {name}",
-            message,
-            email,
-            ['sumams758@gmail.com']
+        mail = EmailMessage(
+            subject=f"Message from {name}",
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[os.getenv('EMAIL_HOST_USER')],
+            reply_to=[email], 
         )
+        mail.send()
     return render(request,'index.html',{'projects':projects})
         
